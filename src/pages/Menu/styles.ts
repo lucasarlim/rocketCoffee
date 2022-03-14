3359b7347b9e366a42d6533069c09c32e2ec9467
Lucas Araújo de Lima <70279700+lucasarlim@users.noreply.github.com>
2022-03-14 17:44:18 -0300

    Fix bug

--- src/pages/Menu/styles.ts
+++ src/pages/Menu/styles.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-  position: relative;
+  max-width: 100vw;
 `
